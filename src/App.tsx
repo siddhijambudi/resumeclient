7eda96d161237f8ccecf7e7ca60ef69af161d95a
siddhijambudi <siddhi.jambudi@gmail.com>
2020-07-31 14:43:49 -0700

    noimplicit any

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ class App extends React.Component<any, any> {
           <img src={logo} className="App-logo" alt="logo"/>
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <PersonDataApp/>
+
       </div>
     );
   }
