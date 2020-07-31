81087d9b3c5ec2f524a4fe1dc09a6777175a35f1
siddhijambudi <siddhi.jambudi@gmail.com>
2020-07-31 11:19:46 -0700

    first

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import './App.css';
+import PersonDataApp from './PersonDataApp';
+
+import logo from './logo.svg';
+
+class App extends React.Component<{}, any> {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo"/>
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <PersonDataApp/>
+      </div>
+    );
+  }
+}
+
+export default App;
